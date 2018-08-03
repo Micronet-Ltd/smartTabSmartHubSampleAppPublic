242d333447f15a12e8bf1b37345b5a81f8fc6df2
Abid <abid.esmail@micronet-inc.com>
2018-08-03 17:58:19 -0600

    MAINT: Adding Micronet license file

--- app/src/main/java/com/micronet/smarttabsmarthubsampleapp/receivers/DeviceStateReceiver.java
+++ app/src/main/java/com/micronet/smarttabsmarthubsampleapp/receivers/DeviceStateReceiver.java
@@ -1,3 +1,8 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
 package com.micronet.smarttabsmarthubsampleapp.receivers;
 
 import android.content.BroadcastReceiver;
